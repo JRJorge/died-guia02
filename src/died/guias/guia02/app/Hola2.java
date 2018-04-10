802eeb541511bff1410be45159b8b288b3857f44
JRJorge <jrjjorge@hotmail.com>
2018-04-10 11:54:04 -0300

    version inicial

---
+++ src/died/guias/guia02/app/Hola2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package died.guias.guia02.app;
+
+/**
+ * @author Jorge
+ *
+ */
+public class Hola2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("el tiempo actual en milis es "+ System.currentTimeMillis());
+
+	}
+
+}
